4590e4c62857400bf9b92f9ec0ae385fa09ba7e7
anonymous-cvpr22-occtyon <1543348204@qq.com>
2021-11-30 10:27:28 +0800

    v1

--- test_w_enhance.py
+++ test_w_enhance.py
@@ -16,7 +16,34 @@ from util import util
 #from pylab import *
 from sklearn.ensemble import IsolationForest
 from PIL import Image
-NC=4
+NC=14
+
+
+def generate_discrete_label(inputs, label_nc, onehot=True, encode=True):
+    pred_batch = []
+    size = inputs.size()
+    for input in inputs:
+        input = input.view(1, label_nc, size[2], size[3])
+        pred = np.squeeze(input.data.max(1)[1].cpu().numpy(), axis=0)
+        pred_batch.append(pred)
+
+    pred_batch = np.array(pred_batch)
+    pred_batch = torch.from_numpy(pred_batch)
+    label_map = []
+    for p in pred_batch:
+        p = p.view(1, 256, 192)
+        label_map.append(p)
+    label_map = torch.stack(label_map, 0)
+    if not onehot:
+        return label_map.float().cuda()
+    size = label_map.size()
+    oneHot_size = (size[0], label_nc, size[2], size[3])
+    input_label = torch.cuda.FloatTensor(torch.Size(oneHot_size)).zero_()
+    input_label = input_label.scatter_(1, label_map.data.long().cuda(), 1.0)
+
+    return input_label
+
+
 def gen_noise(shape):
         noise = np.zeros(shape, dtype=np.uint8)
         ### noise
@@ -102,30 +129,24 @@ warp_model = AFWM(opt, 3)
 print(warp_model)
 warp_model.eval()
 warp_model.cuda()
-load_checkpoint(warp_model, '/PF-AFN-main_XP/PF-AFN_test/checkpoints/PFAFN_warp_epoch.pth')
+load_checkpoint(warp_model, '......./PFAFN_warp_epoch_101.pth')
 
 gen_model = ResUnetGenerator(7, 4, 5, ngf=64, norm_layer=nn.BatchNorm2d)
 print(gen_model)
 gen_model.eval()
 gen_model.cuda()
-load_checkpoint(gen_model,  '/PF-AFN-main_XP/PF-AFN_test/checkpoints/PFAFN_gen_epoch.pth')
+load_checkpoint(gen_model,  '......./PFAFN_gen_epoch_040.pth')
 
-comp_mask_model = UNet1(in_channels=22)
-load_checkpoint(comp_mask_model , '/PF-AFN-main_XP/PF-AFN_test/checkpoints/comp_mask_epoch.pth')
+comp_mask_model = UNet(in_channels=22, n_classes=3)
+load_checkpoint(comp_mask_model , '......./comp_mask_epoch_081.pth')
 comp_mask_model .eval()
 comp_mask_model .cuda()
 
-comp_occu_model = UNet2(in_channels=22)
-load_checkpoint(comp_occu_model , '/PF-AFN-main_XP/PF-AFN_test/checkpoints/comp_sleeve_epoch.pth')
+comp_occu_model = UNet(in_channels=32)
+load_checkpoint(comp_occu_model , '......./comp_sleeve_epoch_final.pth')
 comp_occu_model.eval()
 comp_occu_model.cuda()
 
-
-#refine_model = ResUnetGenerator(6, 4, 5, ngf=64, norm_layer=nn.BatchNorm2d)
-#load_checkpoint(refine_model, '/home/yzj6850/cjy/mixup/checkpoints_v2/flow/comp_occu_model_epoch_021.pth')
-
-#refine_model.eval()
-#refine_model.cuda()
 
 
 
@@ -143,6 +164,7 @@ for epoch in range(1,2):
         clothes = data['clothes']
         t_mask = torch.FloatTensor((data['label'].cpu().numpy() == 7).astype(np.float))
         data['label'] = data['label'] * (1 - t_mask) + t_mask * 4
+        label = data['label'] * (1 - t_mask) + t_mask * 4
         pose = data['pose']
         ##edge is extracted from the clothes image with the built-in function in python
         edge = data['edge']
@@ -153,6 +175,7 @@ for epoch in range(1,2):
         keep_label = torch.FloatTensor((data['label'].cpu().numpy()==11).astype(np.int) + (data['label'].cpu().numpy()==13).astype(np.int)).cuda()
 
         person_clothes_edge = torch.FloatTensor((data['label'].cpu().numpy()==4).astype(np.int))
+        label = label * (1-person_clothes_edge)
         shape = person_clothes_edge.size()
         face = torch.FloatTensor((data['label'].cpu().numpy() == 12).astype(np.int))
         flow_out = warp_model(real_image.cuda(), clothes.cuda())
@@ -168,22 +191,29 @@ for epoch in range(1,2):
 
 
         occu_skin_mask = person_clothes_edge.cuda() * (1 - warped_edge_un)
-        arm_label = torch.FloatTensor((data['label'].cpu().numpy()==11).astype(np.float) + (data['label'].cpu().numpy()==13).astype(np.float)*2).cuda()
+        arm_label = torch.FloatTensor((data['label'].cpu().numpy()==11).astype(np.float) *11+ (data['label'].cpu().numpy()==13).astype(np.float)*13).cuda()
         keep_skin_mask = arm_label * (1-warped_edge_un)
+        label = label.cuda() * (1-warped_edge_un)
         keep_arm_label = (data['label'].cpu().numpy()==11).astype(np.float) + (data['label'].cpu().numpy()==13).astype(np.float)
 
         arm_image = keep_arm_label * real_image.detach().cpu().numpy()
 
         keep_arm_label = torch.FloatTensor(keep_arm_label).cuda()
-
-        occu_w_arm_label = keep_skin_mask + occu_skin_mask*3
+      
+        #occu_w_arm_label = keep_skin_mask + occu_skin_mask*4
+        occu_w_arm_label = label + occu_skin_mask*4
         encode_occu_w_arm_label = encode_input(occu_w_arm_label)
-        gen_arm_mask = comp_occu_model(torch.cat([pose.cuda(), encode_occu_w_arm_label],1))
-        gen_arm_mask = torch.sigmoid(gen_arm_mask)
-        ground, gen_arm_1, gen_arm_2 = torch.split(gen_arm_mask,[1,1,1],1) 
-        gen_arm_1 =  torch.FloatTensor(((gen_arm_1.detach().cpu().numpy() > 0.5)).astype(np.float)).cuda()
-        gen_arm_2 =  torch.FloatTensor(((gen_arm_2.detach().cpu().numpy() > 0.5)).astype(np.float)).cuda() 
-        #clo_dilate = warped_edge_un * keep_arm_label
+        gen_arm_mask1 = comp_occu_model(torch.cat([pose.cuda(), encode_occu_w_arm_label],1))
+        gen_arm_mask = torch.sigmoid(gen_arm_mask1)
+        
+
+        gen_arm_mask = generate_discrete_label(gen_arm_mask.detach(), 14, False)
+
+        gen_arm_1 =  torch.FloatTensor(((gen_arm_mask.cpu().numpy()==11)).astype(np.float)).cuda()
+        gen_arm_2 =  torch.FloatTensor(((gen_arm_mask.cpu().numpy()==13)).astype(np.float)).cuda() 
+        deoccu_w_arm_label = gen_arm_mask * (1-gen_arm_1.cuda())
+        deoccu_w_arm_label = deoccu_w_arm_label  * (1-gen_arm_2.cuda()) 
+        clo_dilate = warped_edge_un * keep_arm_label
         clo_dilate = morpho(warped_edge_un,  8)
         clo_dilate = torch.FloatTensor((clo_dilate.detach().cpu().numpy() > 0.5).astype(np.float32)).cuda()
         gen_arm_1_dilate = gen_arm_1*(1-clo_dilate)
@@ -199,14 +229,14 @@ for epoch in range(1,2):
         gen_arm_1 = gen_arm_1* (1-warped_edge_un)  
         gen_arm_2 = gen_arm_2* (1-warped_edge_un)     
           
-        deoccu_w_arm_label = gen_arm_1*1 + gen_arm_2*2 + warped_edge_un*3
-        encode_deoccu_w_arm_label = encode_input(deoccu_w_arm_label)
+ 
+        deoccu_w_arm_label = gen_arm_1 * 1  +gen_arm_2 * 2 + warped_edge_un*3
+        encode_deoccu_w_arm_label = encode_input(deoccu_w_arm_label, 4)
         deoccu_arm_mask = comp_mask_model (torch.cat([pose.cuda(), encode_deoccu_w_arm_label],1))
         deoccu_arm_mask = torch.sigmoid(deoccu_arm_mask)
-        ground_deoccu, arm_1, arm_2 = torch.split(deoccu_arm_mask,[1,1,1],1) 
-
-        arm_1 =  torch.FloatTensor(((arm_1.detach().cpu().numpy() >0.5)).astype(np.float)).cuda()
-        arm_2 =  torch.FloatTensor(((arm_2.detach().cpu().numpy() >0.5)).astype(np.float)).cuda()
+        deoccu_arm_mask = generate_discrete_label(deoccu_arm_mask.detach(), 3, False)
+        arm_1 =  torch.FloatTensor(((deoccu_arm_mask.cpu().numpy()==1)).astype(np.float)).cuda()
+        arm_2 =  torch.FloatTensor(((deoccu_arm_mask.cpu().numpy()==2)).astype(np.float)).cuda()
 
         warped_edge_un[arm_1==1]=0
         warped_edge_un[arm_2==1]=0
@@ -217,7 +247,6 @@ for epoch in range(1,2):
 
 
         gen_inputs = torch.cat([real_image.cuda(), warped_cloth_un, warped_edge_un], 1)
-        #gen_inputs = torch.cat([real_image.cuda(), warped_cloth, warped_edge], 1)
         gen_outputs = gen_model(gen_inputs)
         p_rendered, m_composite = torch.split(gen_outputs, [3, 1], 1)
         p_rendered = torch.tanh(p_rendered)
@@ -225,84 +254,20 @@ for epoch in range(1,2):
         m_composite = m_composite * warped_edge_un
         p_tryon = warped_cloth_un * m_composite + p_rendered * (1 - m_composite)
         
-        #warped_cloth = p_tryon * warped_edge
-        p_arm = p_tryon.cuda() * gen_arm_label
-        #image_test_arm = image_test.cuda() * gen_arm_label
-        #p_tryon = p_tryon * (1 - keep_label) + real_image.cuda() * keep_label
 
-        #skin_color = ger_average_color(face, face * real_image)
-        #gen_inputs = torch.cat([p_tryon, skin_color],1)
-        #gen_outputs = refine_model(gen_inputs)  
-        #p_rendered, m_composite = torch.split(gen_outputs, [3, 1], 1)
-        #p_rendered = torch.tanh(p_rendered)
-        #m_composite = torch.sigmoid(m_composite)
 
-        #m_composite = m_composite * gen_arm_label
-        #p_tryon1 = p_tryon.cuda() * (1 - m_composite) + p_rendered * m_composite
-        #gen_inputs = torch.cat([p_tryon, keep_left_arm],1)
-        #gen_outputs= refine_model(gen_inputs)
-        #p_rendered, m_composite = torch.split(gen_outputs,[3,1],1)
-        #p_rendered = torch.tanh(p_rendered)
-        #m_composite = torch.sigmoid(m_composite)
-        #m_composite = m_composite * keep_arm_label
-        #p_tryon =p_tryon * (1 - m_composite) + p_rendered * m_composite
+
 
         path = 'results/' + opt.name
         os.makedirs(path, exist_ok=True)
-        sub8_path = 'P_person'
-        sub_path = 'p_gen_un_cloth_W_do/'
-        #sub1_path = 'P_gen_arm/'
-        sub1_path = 'P_enhance_De_occu_test/'
-        sub2_path = 'P_cloth_de_occu_test/'
-        sub3_path = 'P_cloth_init_PFAFN_test/'
-        sub4_path = 'P_cloth_test/'
-        #sub4_path = 'P_reference_arm/'
-        sub5_path = 'P_complex_5/'
-        sub6_path = 'P_cloth_init/'
-        sub7_path = 'P_gen_cloth_init_PFAFN/'
-        os.makedirs(sub_path,exist_ok=True)
-        os.makedirs(sub1_path,exist_ok=True)
-        os.makedirs(sub2_path,exist_ok=True)
-        os.makedirs(sub3_path,exist_ok=True)
-        os.makedirs(sub4_path,exist_ok=True)
-        os.makedirs(sub5_path,exist_ok=True)
-        os.makedirs(sub6_path,exist_ok=True)
-        os.makedirs(sub7_path,exist_ok=True)
+        sub8_path = 'P_person_new'
+
+
         os.makedirs(sub8_path,exist_ok=True)
         #ipdb.set_trace()
         if step % 1 == 0:
-            o = img_deal.float().cuda()
-            a = real_image.float().cuda()
-            b = person_arm_img.float().cuda()
             c = p_tryon.float().cuda()
-            d = clothes.float().cuda()
-            l = warped_cloth_un.float().cuda()
-            f = p_arm.float().cuda()
-            g = warped_edge
-            #d = image_test_arm
-            e = person_arm_img.float().cuda()
-            #d = p_tryon1.float().cuda()
-            #d = generate_label_color(arm_label).float().cuda()
-            #e = generate_label_color(generate_label_plain(encode_occu_w_arm_label)).float().cuda()
-            #f = generate_label_color(generate_label_plain(gen_arm_mask,3)).float().cuda()
-            #g = generate_label_color(generate_label_plain(encode_deoccu_w_arm_label)).float().cuda()
-            #h = generate_label_color(generate_label_plain(deoccu_arm_mask ,3)).float().cuda()
-            k = p_rendered.float().cuda()
-            o = warped_cloth.float().cuda()
-            #l = generate_label_color(need_dilate).float().cuda()
-            #combine = torch.cat([o[0],a[0],b[0],l[0],c[0],d[0],e[0],f[0],g[0],h[0],k[0]], 2).squeeze()
-            #combine = torch.cat([o[0],a[0],b[0],l[0],c[0],d[0],k[0]], 2).squeeze()
-            #cv_img=(combine.permute(1,2,0).detach().cpu().numpy()+1)/2
-            #rgb=(cv_img*255).astype(np.uint8)
-            #bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-            #cv2.imwrite(sub_path+'/'+str(step)+'.jpg',bgr)
-       
-            combine = o[0]
-            #combine = torch.cat([a[0],c[0]], 1).squeeze()
-            cv_img=(combine.permute(1,2,0).detach().cpu().numpy()+1)/2
-            rgb=(cv_img*255).astype(np.uint8)
-            bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-            #cv2.imwrite(sub3_path+'/'+str(step)+'.jpg',bgr)  
+
 
             combine = c[0]
             cv_img=(combine.permute(1,2,0).detach().cpu().numpy()+1)/2
@@ -310,48 +275,10 @@ for epoch in range(1,2):
             bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
             cv2.imwrite(sub8_path+'/'+str(step)+'.jpg',bgr)  
 
-            combine = l[0]
-            cv_img=(combine.permute(1,2,0).detach().cpu().numpy()+1)/2
-            rgb=(cv_img*255).astype(np.uint8)
-            bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-            #cv2.imwrite(sub2_path+'/'+str(step)+'.jpg',bgr)  
-
-            combine = torch.cat([a[0],d[0]], 2).squeeze()
-            cv_img=(combine.permute(1,2,0).detach().cpu().numpy()+1)/2
-            rgb=(cv_img*255).astype(np.uint8)
-            bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-            #cv2.imwrite(sub4_path+'/'+str(step)+'.jpg',bgr)  
-
-            combine = o[0]
-            cv_img=(combine.permute(1,2,0).detach().cpu().numpy()+1)/2
-            rgb=(cv_img*255).astype(np.uint8)
-            bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-            #cv2.imwrite(sub7_path+'/'+str(step)+'.jpg',bgr)  
 
 
-            combine = l[0]
-            cv_img=(combine.permute(1,2,0).detach().cpu().numpy()+1)/2
-            rgb=(cv_img*255).astype(np.uint8)
-            bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-            #cv2.imwrite(sub_path+'/'+str(step)+'.jpg',bgr)  
 
-            combine = b[0]
-            cv_img=(combine.permute(1,2,0).detach().cpu().numpy()+1)/2
-            rgb=(cv_img*255).astype(np.uint8)
-            bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-            #cv2.imwrite(sub4_path+'/'+data['name'][0],bgr)  
 
-            #combine = d[0]
-            #cv_img=(combine.permute(1,2,0).detach().cpu().numpy()+1)/2
-            rgb=(cv_img*255).astype(np.uint8)
-            bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-            #cv2.imwrite(sub3_path+'/'+str(step)+'.jpg',bgr)  
-
-            #combine = d[0]
-            #cv_img=(combine.permute(1,2,0).detach().cpu().numpy()+1)/2
-            #rgb=(cv_img*255).astype(np.uint8)
-            #bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-            #cv2.imwrite(sub2_path+'/'+str(step)+'.jpg',bgr)   
 
         step += 1
         if epoch_iter >= dataset_size:
